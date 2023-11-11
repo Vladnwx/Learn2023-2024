fc7ccc03794a0f1583efb4847a739a528ae7d6d7
Vladimir <anykey196@gmail.com>
2023-11-11 16:04:07 +0500

    timus

---
+++ src/timus/ABProblem.java
@@ -0,0 +1,21 @@
+package timus;
+
+//https://acm.timus.ru/problem.aspx?space=1&num=1000
+
+
+import java.util.Scanner;
+
+public class ABProblem {
+
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b = 0;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println(a+b);
+
+
+    }
+}

e75e65536ea88f1e71f7355066f7e892789df2b2
Vladimir <anykey196@gmail.com>
2023-11-19 22:09:31 +0500

    lr 1 add

--- src/lr1/Example6.java
+++ src/lr1/Example6.java
@@ -3,9 +3,24 @@ package lr1;
 //Напишите программу, в которой Пользователь вводит сначала фамилию, затем имя, затем отчество.
 // После ввода программа выводит сообщение «Hello <фамилия, имя, отчество>».
 
+import java.util.Scanner;
+
 public class Example6 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Введите фамилию: ");
+        String family = in.nextLine();
+
+        System.out.println("Введите имя: ");
+        String name = in.nextLine();
+
+        System.out.println("Введите отчество: ");
+        String otchestvo = in.nextLine();
+
+        System.out.println("Hello " + family + " "+ name + " " + otchestvo);
+
+        in.close();
 
     }
 }

015f60ac185fedc78b4baa6c913c1bb148cfa1cc
Vladimir <anykey196@gmail.com>
2023-11-19 22:16:23 +0500

    ex7

--- src/lr1/Example7.java
+++ src/lr1/Example7.java
@@ -1,8 +1,24 @@
 package lr1;
+
+import java.util.Scanner;
+
 //Напишите программу, в которой Пользователь вводит имя и возраст. Программа отображает сообщение об имени и возрасте пользователя.
 public class Example7 {
 
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите имя:  ");
+
+        String name = in.nextLine();
+
+        System.out.println("Введите возраст:  ");
+
+        int age = in.nextInt();
+
+        System.out.println("Ваше имя: "+name +"\n" + "Ваш возраст: " + age);
+        in.close();
 
     }
 }

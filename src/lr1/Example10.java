1d8d2bc01cf01a5b69217232cbfd75eae5175f1b
Vladimir <anykey196@gmail.com>
2023-11-19 22:10:56 +0500

    psvm add

--- src/lr1/Example10.java
+++ src/lr1/Example10.java
@@ -1,4 +1,7 @@
 package lr1;
 //Напишите программу, в которой по году рождения определяется возраст пользователя.
 public class Example10 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/lr1/Example12.java
+++ src/lr1/Example12.java
@@ -1,4 +1,7 @@
 package lr1;
 //Напишите программу, в которой по возрасту определяется год рождения.
 public class Example12 {
+    public static void main(String[] args) {
+
+    }
 }

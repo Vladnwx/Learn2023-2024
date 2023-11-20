9de31da512fbdd7b2998767713960ed1194d78fc
Vlad <anykey196@gmail.com>
2023-11-20 19:14:14 +0500

    Update Example8.java

--- src/lr1/Example8.java
+++ src/lr1/Example8.java
@@ -6,5 +6,24 @@ public class Example8 {
 
     public static void main(String[] args) {
 
+Scanner in = new Scanner(System.in);
+
+System.out.println("Введите день недели");
+
+String nameDay = in.nextline();
+
+
+System.out.println("Введите месяц");
+
+String mounth = in.nextline();
+
+System.out.println("Введите номер дня в месяце");
+
+String day = in.nextline();
+
+System.out.println(nameDay +" " + day +" " +mounth);
+ 
+in.close();
+
     }
 }

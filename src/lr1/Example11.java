889194d6a803f8ca0566625713081a2ee32b65c2
Vlad <anykey196@gmail.com>
2023-11-20 19:39:48 +0500

    Update Example11.java

--- src/lr1/Example11.java
+++ src/lr1/Example11.java
@@ -3,5 +3,21 @@ package lr1;
 public class Example11 {
     public static void main(String[] args) {
 
+Scanner in = new Scanner (System.in);
+
+System.out.println("Введите имя: ");
+
+String name = in.nextline();
+
+System.out.println("Введите возраст: ");
+
+int age = in.nextInt();
+
+System.out.println("Ваши данные: " + name +" " + age);
+in.close();
+
+
+
+
     }
 }

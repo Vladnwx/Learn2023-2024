d7b2dfe72dcf3297626b7be95720925935aa27ec
Vlad <anykey196@gmail.com>
2023-11-20 21:38:31 +0500

    Update Example15.java

--- src/lr1/Example15.java
+++ src/lr1/Example15.java
@@ -3,5 +3,20 @@ package lr1;
 public class Example15 {
     public static void main(String[] args) {
 
+Scanner in = new Scanner (System.in);
+
+System.out.println("Введите первое число: ");
+
+int a = in.nextInt();
+
+System.out.println("Введите второе число: ");
+
+int b = in.nextInt();
+
+System.out.println("Сумма чисел: " + (a+b);
+
+System.out.println("Разность чисел: " + (a-b);
+
+in.close();
     }
 }

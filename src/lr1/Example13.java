94a576d14ef0a6fc7179aecb354abd038568f553
Vlad <anykey196@gmail.com>
2023-11-20 21:16:53 +0500

    Update Example13.java

--- src/lr1/Example13.java
+++ src/lr1/Example13.java
@@ -3,5 +3,19 @@ package lr1;
 public class Example13 {
     public static void main(String[] args) {
 
+Scanner in = new Scanner(System.in);
+
+System.out.println("Введите первое число: ");
+
+int a = in.nextInt();
+
+System.out.println("Введите второе число: ");
+
+int b = in.nextInt();
+
+System.out.println("Сумма= " + (a+b);
+
+in.close();
+
     }
 }

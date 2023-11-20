1f260a77adf2018708b71be2d1255f3d8705d24f
Vlad <anykey196@gmail.com>
2023-11-20 19:20:58 +0500

    Update Example9.java

--- src/lr1/Example9.java
+++ src/lr1/Example9.java
@@ -4,5 +4,21 @@ package lr1;
 public class Example9 {
     public static void main(String[] args) {
 
+Scanner in = new Scanner(System.in);
+
+System.out.println("Введите название месяца: ");
+
+String s = in.nextline();
+
+System.out.println("Введите количество дней в месяце: ");
+
+int i= in.nextInt();
+
+System.out.println( "Месяц " +s +" " +"Содержит " +i +" дней" );
+in.close();
+
+
+
+
     }
 }

ffe94b09870d49a13fa75a3cbb3a6ace72f5fc06
Vlad <anykey196@gmail.com>
2023-11-20 21:29:37 +0500

    Update Example14.java

--- src/lr1/Example14.java
+++ src/lr1/Example14.java
@@ -5,5 +5,18 @@ package lr1;
 public class Example14 {
     public static void main(String[] args) {
 
+Scanner in = new Scanner(System.in);
+
+System.out.println("Введите число: ");
+
+int a=in.nextInt();
+
+System.out.println(a-1);
+System.out.println(a);
+System.out.println(a+1);
+System.out.println(3*a*3*a);
+
+in.close();
+
     }
 }

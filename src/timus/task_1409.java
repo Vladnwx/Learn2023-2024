09b5a1bbafd0fa34b3adab932e2ffa15bd4fceb6
Vlad <anykey196@gmail.com>
2023-11-21 09:30:33 +0500

    Update task_1409.java

--- src/timus/task_1409.java
+++ src/timus/task_1409.java
@@ -1,1 +1,23 @@
-// task 1409
+import java.util.Scanner;
+
+//JUDGE_ID 368360HH
+
+//
+
+/*
+
+
+
+*/
+
+public class task_1409 {
+    public static void main(String[] args){
+
+Scanner in = new Scanner(System.in);
+
+System.out.println("Hello world");
+in.close();
+
+
+      }
+    }

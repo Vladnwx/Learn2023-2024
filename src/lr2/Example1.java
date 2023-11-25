79f3112845a901ff0681d2bde9211c61e24fdb13
Vladimir <anykey196@gmail.com>
2023-11-25 20:10:50 +0500

    lr2 example 1 add solution

---
+++ src/lr2/Example1.java
@@ -0,0 +1,21 @@
+package lr2;
+//Напишите программу, которая проверяет, делится ли введенное Пользователем число на 3.
+import java.util.Scanner;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        {System.out.println("Введите целое число");}
+        Scanner in = new Scanner(System.in);
+        long l;
+        if (in.hasNextLong()){
+            l=in.nextLong();
+            l= l%3;
+        }
+        else {System.out.println("Вы ввели не целое число, программа будет закрыта");
+        return;}
+        if (l==0){System.out.println("Число делится на 3");}
+        else {System.out.println("Число НЕ делится на 3");}
+
+    }
+}

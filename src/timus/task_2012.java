b70e10fab2cf02cf4b90322c06762596a9ba99a8
Vlad <anykey196@gmail.com>
2023-11-25 11:08:49 +0500

    Update task_2012.java

--- src/timus/task_2012.java
+++ src/timus/task_2012.java
@@ -1,1 +1,18 @@
-\\
+package timus.task_2012;
+
+//JUDGE_ID 368360HH
+
+//https://acm.timus.ru/problem.aspx?space=1&num=2012
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 0;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        System.out.println(a);
+in.close();
+    }
+}

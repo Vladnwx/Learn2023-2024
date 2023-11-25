609898c157caf7fee3410298d7e0f11323ac4103
Vladimir <anykey196@gmail.com>
2023-11-25 20:27:35 +0500

    lr2 example3 add solution

---
+++ src/lr2/Example3.java
@@ -0,0 +1,24 @@
+package lr2;
+
+import java.util.Scanner;
+
+//Напишите программу, которая проверяет, удовлетворяет ли
+// введенное пользователем число следующим критериям:
+// число делится на 4, и при этом оно не меньше 10.
+public class Example3 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        long l;
+        if (in.hasNextLong()){
+            l=in.nextLong();
+            if (l%4==0 & l>10){
+                System.out.println("Число удовлетворяет следующим критериям: число делится на 4, и при этом оно не меньше 10");
+            }
+            else {System.out.println("Число НЕ удовлетворяет следующим критериям: число делится на 4, и при этом оно не меньше 10");}
+        }
+        else {
+            System.out.println("Вы ввели не целое число");
+        }
+
+    }
+}

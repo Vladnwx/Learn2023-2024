3ce1a0312464bf569cb789adf49da9a7cb8b1b14
Vladimir <anykey196@gmail.com>
2023-11-25 20:32:28 +0500

    lr2 example4 add solution

---
+++ src/lr2/Example4.java
@@ -0,0 +1,25 @@
+package lr2;
+
+import java.util.Scanner;
+
+//Напишите программу, которая проверяет, попадает ли введение пользователем число в диапазон от 5 до 10 включительно.
+public class Example4 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int i;
+        if (in.hasNextInt()){
+            i=in.nextInt();
+            if (i>=5 & i<=10){
+                System.out.println("Введённое пользователем число ПОПАДАЕТ в диапазон от 5 до 10 включительно");
+            }
+            else {System.out.println("Введённое пользователем число НЕ попадает в диапазон от 5 до 10 включительно");}
+
+        }
+        else {
+            System.out.println("Вы ввели не целое число");
+        }
+
+    }
+}

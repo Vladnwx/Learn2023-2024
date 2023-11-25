a23c61a528d7ac8dd84da638fbfeb248ec11a699
Vlad <anykey196@gmail.com>
2023-11-25 11:40:44 +0500

    Update task_1877.java

--- src/timus/task_1877.java
+++ src/timus/task_1877.java
@@ -10,9 +10,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);    
 int a = in.nextInt();
+int b = in.nextInt();
 
-
-System.out.println("NO");
+if(a>0 & a%2==0) {
+System.out.println("yes");
+in.close();
+return 0;
+} 
+else if (a==0){
+System.out.println("yes");
+in.close();
+return 0;
+}
+else if(b>0 & b%2>0) {
+System.out.println("yes");
+in.close();
+return 0;
+}
+else if (b==1) {
+System.out.println("yes");
+in.close();
+return 0;
+}
+else System.out.println("NO");
 in.close();
     }
 }

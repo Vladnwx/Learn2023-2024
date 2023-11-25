dcd37716389066ceefd78a185acee8839f7af5b8
Vlad <anykey196@gmail.com>
2023-11-25 11:23:09 +0500

    Update task_1877.java

--- src/timus/task_1877.java
+++ src/timus/task_1877.java
@@ -1,1 +1,18 @@
-\\
+package timus.task_1877;
+
+//JUDGE_ID 368360HH
+
+//https://acm.timus.ru/problem.aspx?space=1&num=1877
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);    
+int a = in.nextInt();
+
+
+System.out.println("NO");
+in.close();
+    }
+}

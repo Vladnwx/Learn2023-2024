b4cac928d6d48816c9350ec289577fe0df96513e
Vladimir <anykey196@gmail.com>
2023-11-26 10:33:52 +0500

    timus

--- src/timus/task_1000/Main.java
+++ src/timus/task_1000/Main.java
@@ -7,17 +7,12 @@ package timus.task_1000;
 
 //https://acm.timus.ru/problem.aspx?space=1&num=1000
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        int a = 0;
-        int b = 0;
+        int a, b;
         Scanner in = new Scanner(System.in);
         a = in.nextInt();
         b = in.nextInt();

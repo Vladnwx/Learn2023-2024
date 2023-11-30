5707d0eb07d926d528705a6418d78ad02acac12d
Vladimir <anykey196@gmail.com>
2023-11-30 22:06:02 +0500

    Добавление условий задач

--- src/lr3/Example3.java
+++ src/lr3/Example3.java
@@ -1,4 +1,9 @@
 package lr3;
+
+import org.graalvm.nativeimage.c.struct.SizeOf;
+
+import java.util.Scanner;
+
 /*
 Напишите программу, которая выводит последовательность чисел Фибоначчи.
 Первые два числа в этой последовательности равны 1,
@@ -9,6 +14,56 @@ package lr3;
  */
 public class Example3 {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        long a=0, b=0, c=0, result;
+
+        System.out.println("Введите целое число - количество цифр в последовательности Фибонначи");
+        if (in.hasNextLong())
+        {
+            a= in.nextLong();
+        } else {System.out.println("Вы ввели не чиcло или число выходящее за" + "9 223 372 036 854 775 807");
+            return;
+        }
+        System.out.println("Числа Фибоначчи черз for");
+        for (int i=0; i<=a; i++)
+        {
+            if (i>2)
+            {
+                result = b+c;
+                System.out.print("," + result );
+                b=c;
+                c=result;
+
+            }
+            else {
+                if (i==0) System.out.print(1);
+                if(i==1) {System.out.print("," +1);
+                b=1;
+                c=1;}
+            }
+        }
+        System.out.println("\nЧисла Фибоначчи черз while");
+        int i=0;
+        while (i<=a){
+            if (i>2)
+            {
+                result = b+c;
+                System.out.print("," + result );
+                b=c;
+                c=result;
+                i++;
+            }
+            else {
+                if (i==0) {System.out.print(1);};
+                if(i==1) {System.out.print("," +1);
+                    b=1;
+                    c=1;
+                }
+                i++;
+            }
+        }
+
 
     }
 }

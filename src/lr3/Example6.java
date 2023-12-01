30d83d4398e03db72e269a030d1365a2eb3f26ce
Vladimir <anykey196@gmail.com>
2023-12-01 09:27:15 +0500

    Промежуточное решение

--- src/lr3/Example6.java
+++ src/lr3/Example6.java
@@ -1,4 +1,7 @@
 package lr3;
+
+import java.util.Scanner;
+
 /*
 Напишите программу, в которой создается одномерный числовой массив и
 заполняется числами, которые при делении на 5 дают в остатке 2
@@ -7,6 +10,17 @@ package lr3;
  */
 public class Example6 {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int a;
+        System.out.println("Введите размер массива:");
+        if (in.hasNextInt()) {
+            a = in.nextInt();
+        } else {
+            System.out.println("Введено некорректное значение");
+            return;
+        }
+
 
     }
 }

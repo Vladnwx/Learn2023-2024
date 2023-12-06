896df17a69d0d9fb815d1731b21aa969d7c65354
Vlad <anykey196@gmail.com>
2023-12-06 23:16:56 +0500

    Update Main.java

--- src/timus/task_1068/Main.java
+++ src/timus/task_1068/Main.java
@@ -1,5 +1,19 @@
 package timus.task_1068;
 
+//JUDGE_ID 368360HH
+//https://acm.timus.ru/problem.aspx?space=1&num=1068
+/*
+1068. Сумма
+Ограничение времени: 2.0 секунды
+Ограничение памяти: 64 МБ
+Всё, что от вас требуется — найти сумму всех целых чисел, лежащих между 1 и N включительно.
+Исходные данные
+В единственной строке расположено число N, по модулю не превосходящее 10000.
+Результат
+Выведите целое число — ответ задачи.
+
+*/
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -17,5 +31,20 @@ public class Main {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        int a, sum=0;
+a=in.nextInt();
+if (a<=0) {
+for (int )
+
+}
+else { 
+
+}
+System.out.println(sum);
+
+
+   
+
+
     }
 }

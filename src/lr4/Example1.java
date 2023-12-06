efa8202c0d19749a60bd4b08f3b76e86e63809d0
Vlad <anykey196@gmail.com>
2023-12-06 16:12:49 +0500

    Решение задачи

--- src/lr4/Example1.java
+++ src/lr4/Example1.java
@@ -9,7 +9,27 @@ import java.util.Arrays;
 public class Example1 {
     public static void main(String[] args) {
 
+int a=23, b=11;
 
+for (int i=0; i<b;i++) {
+if (i==0 || i==b-1) {
+for (int j=0; j<a; j++) {
+System.out.print("-");
+}
+} else {
+    System.out.println("\n");
+    System.out.print("|");
+    for (int k=0; k<a; k++) {
+        if (k==a-1) {
+        System.out.print("| \n"); 
+                        
+        } else System.out.print(" ");
+    }
+    
+}
+
+}
+        
 
     }
 

f2009a12950c62ae14a60aedfa37f45f299d2b73
Vladimir <anykey196@gmail.com>
2023-12-06 19:50:41 +0500

    Решение задачи

--- src/lr4/Example2.java
+++ src/lr4/Example2.java
@@ -3,4 +3,31 @@ package lr4;
 Напишите программу, которая выводит в консольное окно прямоугольный треугольник;
  */
 public class Example2 {
+    public static void main(String[] args) {
+        int a=10;
+
+        for (int i=0; i<a; i++){
+            switch (i) {
+                case 0:
+                    System.out.println("|\\");
+                    break;
+                default:
+                    System.out.print("|");
+                    for (int j=0; j<i; j++){
+                    System.out.print(" ");}
+                    System.out.print("\\");
+                    System.out.print("\n");
+                    break;
+                    case 9:{
+                    for (int j=0; j<a;j++) {
+                        System.out.print("-");
+                    }
+                    break;}
+
+
+            }
+        }
+
+
+    }
 }

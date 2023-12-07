2bac4ed955a6d93ba82df8a1e638cb7544948f66
Vlad <anykey196@gmail.com>
2023-12-07 13:00:48 +0500

    Update Example9.java

--- src/lr4/Example9.java
+++ src/lr4/Example9.java
@@ -6,29 +6,46 @@ package lr4;
  */
 public class Example9 {
 
-    public static void main(String[] args) {
-
+  
+  public static void main(String[] args) {
+        
       Scanner in = new Scanner (System.in);
         System.out.println("Введите текс для шифрования:");
         String s = "";
         s = in.next();
         System.out.println("Введите ключ:");
         int key = in.nextInt();
-        if (key>255){
-            key-=255;
+        while (key>256){
+            key-=256;
         }
+        
         int l=s.length();
+        
         char[] c = new char[l];
+        
+        //Генерация алфавита
+        
+        Random random = new Random();
+            char [] alphavit = new char [256];
+            for (int i=0; i<256; i++)
+        {
+            alphavit[i]= (char) random.nextInt(256);
+        }
+        
+        //System.out.println(Arrays.toString(alphavit));
+            
+        
+        
         System.out.println("Текст после преобразования:");
         for (int i=0; i<l; i++){
             c[i] = s.charAt(i);
-            int tmp = c[i];
-            tmp +=key;
+            int tmp;
+            tmp = c [i] + alphavit [i] +key;
             c[i] = (char) tmp;
             System.out.print(c[i]);
         }
         System.out.println("\nВыполнить обратное преобразование");
-
+        
         char c1;
         while (true){
             System.out.println("Введите \"y\" или \"n\" :");
@@ -38,8 +55,8 @@ public class Example9 {
                 System.out.println("\nТекст после преобразования");
                // System.out.println(s);
                 for (int i=0; i<l; i++){
-            int tmp = c[i];
-            tmp -=key;
+            int tmp;
+            tmp = c[i] - alphavit[i] -key;
             c[i] = (char) tmp;
             System.out.print(c[i]);
         }
@@ -52,11 +69,10 @@ public class Example9 {
             }
             else System.out.println("Введите корректный ответ");
         }
-
+        
       //  System.out.println("\nДешифрованный текст");
-
+        
     }
-
 
 
 }

d2f2ab98aa8c9962a12e8599d07e8526d16ffb33
Vlad <anykey196@gmail.com>
2023-12-07 10:40:01 +0500

    Update Example9.java

--- src/lr4/Example9.java
+++ src/lr4/Example9.java
@@ -5,7 +5,58 @@ package lr4;
 остальные условия идентичны задаче 8.
  */
 public class Example9 {
+
     public static void main(String[] args) {
 
+      Scanner in = new Scanner (System.in);
+        System.out.println("Введите текс для шифрования:");
+        String s = "";
+        s = in.next();
+        System.out.println("Введите ключ:");
+        int key = in.nextInt();
+        if (key>255){
+            key-=255;
+        }
+        int l=s.length();
+        char[] c = new char[l];
+        System.out.println("Текст после преобразования:");
+        for (int i=0; i<l; i++){
+            c[i] = s.charAt(i);
+            int tmp = c[i];
+            tmp +=key;
+            c[i] = (char) tmp;
+            System.out.print(c[i]);
+        }
+        System.out.println("\nВыполнить обратное преобразование");
+
+        char c1;
+        while (true){
+            System.out.println("Введите \"y\" или \"n\" :");
+            c1 = (char) in.next().charAt(0);
+        //System.out.println(c);
+            if (c1 == 'y'){
+                System.out.println("\nТекст после преобразования");
+               // System.out.println(s);
+                for (int i=0; i<l; i++){
+            int tmp = c[i];
+            tmp -=key;
+            c[i] = (char) tmp;
+            System.out.print(c[i]);
+        }
+                System.out.println("\n");
+                return;
+               // break;
+            } else if(c1== 'n') {
+                System.out.println("До свидания!");
+            return;
+            }
+            else System.out.println("Введите корректный ответ");
+        }
+
+      //  System.out.println("\nДешифрованный текст");
+
     }
+
+
+
 }

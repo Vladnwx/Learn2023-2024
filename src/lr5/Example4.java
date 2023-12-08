36c36a988ea7b5405fbf829b0a18b222bd4b595c
Vlad <anykey196@gmail.com>
2023-12-08 15:04:37 +0500

    Update Example4.java

--- src/lr5/Example4.java
+++ src/lr5/Example4.java
@@ -1,1 +1,41 @@
-//
+import java.util.Random;
+import java.lang.Math;
+
+public class Main
+{
+    public static class CharAndInt{
+        private int i;
+        private char c;
+ 
+ CharAndInt (){
+     Random random = new Random();
+     i = random.nextInt(1000);
+     c = 'a';
+ }
+ CharAndInt (double d){
+     this.i =  (int) Math.floor(Math.abs(i));
+     String s = String.valueOf(d%1);
+    // System.out.println(s);
+     s = s.substring(2, 4);
+    // System.out.println(s);
+     int k = (int) Integer.parseInt(s);
+     this.c = (char) k;
+ }
+ CharAndInt (int i, char c){
+     Random random = new Random();
+     this.i = i;
+     this.c = c;
+ }
+ public void Print (){
+     System.out.println("INT = " + i + " CHAR = " + c);
+ }
+    }
+  public static void main(String[] args) {
+  CharAndInt charAndInt1 = new CharAndInt();
+  charAndInt1.Print();
+  CharAndInt charAndInt2 = new CharAndInt(112.75);
+  charAndInt2.Print();
+  CharAndInt charAndInt3 = new CharAndInt(2, 'T');
+  charAndInt3.Print();
+  }
+}

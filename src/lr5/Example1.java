5330cdcd6928bf2274537c36c74f546ce6927d63
Vlad <anykey196@gmail.com>
2023-12-08 10:29:55 +0500

    Update Example1.java

--- src/lr5/Example1.java
+++ src/lr5/Example1.java
@@ -1,1 +1,51 @@
 //
+public static class User {
+        
+        private char symbol;
+        
+        public void setSymbol (String symbol)
+        {
+            this.symbol = (char) symbol.charAt(0);
+        }
+        
+        public char getSymbol ()
+        {
+            return symbol;
+        }
+        
+         public void printSymbol ()
+        {
+            System.out.println("Символ: " + getSymbol());
+        }
+        
+         public void printValue ()
+        {
+            System.out.println("Код символа: " + (int) getSymbol());
+        }
+        
+        
+        public int getValue ()
+        {
+            return (int) symbol;
+        }
+        
+        
+        public void printSymbolAndValue ()
+        {
+            System.out.println("Символ: " + getSymbol() + " Код символа: "  + (int) getSymbol());
+        }
+        
+        
+    }
+  
+  
+  public static void main(String[] args) {
+    
+    
+    
+    User user = new User();
+    user.setSymbol("a");
+    user.printSymbol();
+    user.printSymbolAndValue();
+    
+  }

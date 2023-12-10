f408e73fca0cee12caf87bfdaed07a3b57dbd1fa
Vladimir <anykey196@gmail.com>
2023-12-10 11:57:20 +0500

    Решение задачи №1

--- src/lr6/Example1.java
+++ src/lr6/Example1.java
@@ -12,4 +12,55 @@ package lr6;
 формируется текстовая строка и присваивается значением текстовому полю.
  */
 public class Example1 {
+
+    private static class TextWrapper{
+
+        char c;
+        String s;
+
+        public void setTextWrapper(char c){
+            this.c =c;
+        }
+        public void setTextWrapper(String s){
+            this.s =s;
+        }
+
+        public void setTextWrapper(char[] c){
+            if (c.length==1){
+                this.c =c[0];
+            } else {
+                this.s =String.valueOf(c);
+            }
+        }
+
+        public String getc (){
+            return String.valueOf(c);
+        }
+        public String gets (){
+            return s;
+        }
+
+        public void getAll(){
+            System.out.println("c = " + getc() + "\n" + "s = " + gets());
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Передача символа");
+        TextWrapper textWrapper1 = new TextWrapper();
+        textWrapper1.setTextWrapper('a');
+        textWrapper1.getAll();
+
+        System.out.println("Передача строки");
+        TextWrapper textWrapper2 = new TextWrapper();
+        textWrapper2.setTextWrapper("ЭТО СТРОКА");
+        textWrapper2.getAll();
+
+        System.out.println("Передача массива символов");
+        char [] arrchar = {'Э', 'Т','О',' ','М','А','С','С','И','В',' ','С','И','М','В','О','Л','О','В'};
+        TextWrapper textWrapper3 = new TextWrapper();
+        textWrapper3.setTextWrapper(arrchar);
+        textWrapper3.getAll();
+
+    }
 }

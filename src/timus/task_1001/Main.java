495d7eefc7b866651474692d14623e8e4226176a
Vladimir <anykey196@gmail.com>
2023-12-16 16:11:59 +0500

    Решение задачи 1001

--- src/timus/task_1001/Main.java
+++ src/timus/task_1001/Main.java
@@ -4,10 +4,10 @@ package timus.task_1001;
 
 //https://acm.timus.ru/problem.aspx?space=1&num=1001
 
-import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,49 +15,22 @@ public class Main {
         String inputFileName = "src/timus/task_1001/input.txt";
         boolean oj = System.getProperty("ONLINE_JUDGE") != null;
 
-        byte[] bytes =  System.in.readNBytes(256*1024);
-        System.out.println("Text [Byte Format] : " + bytes);
-  /*      try{
-            BufferedReader bufferedReader =
-                    oj ? new BufferedReader(new InputStreamReader(System.in)) :
-                            new BufferedReader(new FileReader(inputFileName));
-            String readLine ="";
-            String out ="";
+        Scanner in = null;
 
-            int i=0;
-          while(i<100) {
-               readLine = bufferedReader.readLine();
+        try {
+            in = oj ? new Scanner(System.in) : new Scanner(new FileReader(inputFileName));
 
-              if (readLine != null) {
-                  readLine = readLine.replaceAll("\\s+", " ");
-                  out = out + " " + readLine;
-                  i++;
-              }
-              else i++;
-          }
-            out = out.replaceAll("\\s+", " ");
-            String[] strings = out.split(" ", 100);
-            int ccc =strings.length;
-            double [] nums = new double[100];
-            for (int c=0; c<strings.length;c++) {
-                if (strings[c].isEmpty()){
-                    nums[c] = -1.0D;
-                    continue;
-                }
-                else {
-                    nums[c] = Math.sqrt(Double.parseDouble(strings[c]));
-                }
-            }
-
-            while(ccc>0) {
-                if(nums[ccc-1]>=0){
-                String s = String.format("%.4f",nums[ccc-1] );
-                System.out.println(s);
-                ccc--;}
-                else ccc--;
-            }
-        } catch (IOException exception){
-            exception.printStackTrace();
-        }*/
+        } catch (FileNotFoundException e){
+            throw new RuntimeException(e);
+        }
+        double[] d = new double [256*1024];
+        int c=0;
+        while (in.hasNextLong()){
+            d[c] = Math.sqrt(in.nextLong());
+            c++;
+        }
+        for (int i=c-1; i>=0; i--){
+            System.out.println(String.format("%.4f", d[i]));
+        }
     }
 }
--- src/timus/task_1100/Main.java
+++ src/timus/task_1100/Main.java
@@ -5,8 +5,6 @@ package timus.task_1100;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -22,49 +20,38 @@ public class Main {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        int N;
+        N = in.nextInt();
+        int[][] arr = new int[N][2];
 
-        int a;
-
-        a = in.nextInt();
-        int[][] arr = new int[a][2];
-        for (int i =0; i<a;i++){
-                arr[i][0] = in.nextInt();
-                arr[i][1] = in.nextInt();
+        for (int i =0; i<N;i++) {
+            arr[i][0] = in.nextInt();
+            arr[i][1] = in.nextInt();
         }
 
-        int max=0, min=0;
+        
 
-        for (int i =0; i<a; i++)
-        {
-            if (arr[i][1]>max){
-                max=arr[i][1];
-            }
-            else if (arr[i][1]<min){
-            min=arr[i][1];
-                                    }
-        }
+  /*      int count=N;
 
+      while (count>0){
+          for (int i=0; i<count-1; i++){
+              if (arr[i][1]<arr[i+1][1]){
+                  int tmp1 = arr[i+1][1];
+                  int tmp2 = arr[i+1][0];
+                  arr[i+1][1] = arr[i][1];
+                  arr[i+1][0] = arr[i][0];
+                  arr[i][1] = tmp1;
+                  arr[i][0] = tmp2;
+              } else continue;
+          }
+          count--;
 
-        for (int i=0; i<a; i++){
+      }
+
+*/
+        for (int i=0; i<N; i++){
             System.out.print(arr[i][0]+ " "+ arr[i][1] + "\n");
         }
-
-
-
-
-
-
-        Integer [] arr2 = new Integer[a];
-        for (int i =0; i<a; i++){
-            arr2[i]=arr[i][1];
-        }
-        Arrays.sort(arr2, Collections.reverseOrder());
-        System.out.println(Arrays.toString(arr2));
-
-        for (int i=0; i<arr2.length; i++){
-
-        }
-
 
     }
 }

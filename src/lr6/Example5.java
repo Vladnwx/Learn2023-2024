4ffdb11b24109fe60e7cab7a7fab88617cc555f0
Vladimir <anykey196@gmail.com>
2024-01-04 23:22:04 +0500

    Решение задачи 5

--- src/lr6/Example5.java
+++ src/lr6/Example5.java
@@ -8,7 +8,27 @@ package lr6;
  */
 public class Example5 {
 
+    public static long summaSquare(int n){
+       int i=1;
+       long result=0;
+       while (i<=n){
+           result = result+i*i;
+           i++;
+       }
+
+       return result;
+    }
+
     public static void main(String[] args) {
+
+        int n=8; //Числонатуральных чисел
+
+        System.out.println("Вычисление: ");
+        System.out.println(summaSquare(n));
+
+        System.out.println("Проверка по формуле: ");
+        int j=n*(n+1)*(2*n+1)/6;
+        System.out.println(j);
 
     }
 }

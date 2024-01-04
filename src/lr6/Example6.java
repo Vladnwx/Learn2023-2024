a9eed1ed1454921060012ae3b56684e8d235e85b
Vladimir <anykey196@gmail.com>
2024-01-04 23:40:42 +0500

    Решение задачи 6

--- src/lr6/Example6.java
+++ src/lr6/Example6.java
@@ -1,4 +1,8 @@
 package lr6;
+
+import java.util.Arrays;
+import java.util.Random;
+
 /*
 Напишите программу со статическим методом, которому аргументом передается
 целочисленный массив и целое число. Результатом метод возвращает ссылку
@@ -11,7 +15,36 @@ package lr6;
  */
 public class Example6 {
 
+    public static int[] arrResize (int[] arrSource, int c){
+        int [] arrresult = new int[c];
+if (arrSource.length>c){
+    for (int i=0; i<c;i++){
+        arrresult[i]=arrSource[i];
+    }
+} else arrresult = arrSource.clone();
+        return arrresult;
+
+    }
+
+
     public static void main(String[] args) {
+        Random random = new Random();
+        int r = random.nextInt(5);
+        int [] arrSource = new int[r];
+        for (int i=0; i<r;i++){
+            arrSource[i] = random.nextInt(1000);
+        }
+
+        int newSize = 10;
+
+        System.out.println("Ссылка на массив новый: " + arrResize(arrSource, newSize));
+        System.out.println("Состав массива:");
+        System.out.println(Arrays.toString(arrResize(arrSource, newSize)));
+
+        System.out.println("Ссылка на массив исходный: " + arrSource);
+        System.out.println("Состав массива:");
+        System.out.println(Arrays.toString(arrSource));
+
 
     }
 }

cf583960558846eebfb2b35d38194ded373fa761
Vlad <anykey196@gmail.com>
2024-01-05 15:03:33 +0500

    Update Example9.java

--- src/lr6/Example9.java
+++ src/lr6/Example9.java
@@ -5,9 +5,42 @@ package lr6;
 элементы массива попарно меняются местами: первый — с последним,
 второй — с предпоследним и так далее.
  */
+import java.util.Random;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Example9 {
+    
+     static boolean CHEATCODE = true;
+  
+  public static void reverseArray(char[] arrChar){
+   if (CHEATCODE){
+       
+       Collections.reverse(Arrays.asList(arrChar));
+       
+   } 
+   else {
+       
+       
+   }
 
-    public static void main(String[] args) {
+  }
+  
+  public static void main(String[] args) {
+      
+Random random = new Random();
+int r = random.nextInt(50);
 
-    }
+char[] arrChar = new char[r];
+
+for (int i =0; i<arrChar.length; i++){
+    arrChar[i] = (char) random.nextInt(1000);
 }
+System.out.println("Массив символов: \n" + Arrays.toString(arrChar));
+//reverseArray(arrChar);
+ Collections.reverse(Arrays.asList(arrChar));
+System.out.println("Массив символов перевернутый : \n" + Arrays.toString(arrChar));
+  
+  }
+}
+
